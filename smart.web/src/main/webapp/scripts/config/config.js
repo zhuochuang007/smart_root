27537a23ca777cbf2f53cf8b9271d0f6e2876f76
chuzhisheng <399608459@qq.com>
2016-05-27 14:14:08 +0800

    修改js配置文件格式

--- smart.web/src/main/webapp/scripts/config/config.js
+++ smart.web/src/main/webapp/scripts/config/config.js
@@ -1,14 +1,12 @@
 require.config({
-　　　　baseUrl: "scripts",
-　　　　paths: {
-　　　　　　jquery: "vendor/jquery-1.12.4.min",
-　　　　　　bootstrap: "vendor/bootstrap.min",
-        angular:"vendor/angular.min"
-　　　　},
+	 baseUrl: "scripts",
+	 paths: {
+		     jquery:"vendor/jquery-1.12.4.min",
+		     bootstrap:"vendor/bootstrap.min",
+		     angular:"vendor/angular.min"
+		    },
 	 shim: {
-	    'angular': {
-	        exports: 'angular'
-	    }
-	 }
-　　});
+		    'angular': {exports: 'angular'}
+		   }
+	});
 require(['jquery', 'bootstrap','angular'],function($, canvas, sub) {});
